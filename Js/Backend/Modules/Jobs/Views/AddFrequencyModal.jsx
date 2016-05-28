a5a662e4e14ac60e8ec7dd075c37345641805a37
Pavel910 <pavel@webiny.com>
2016-05-28 21:29:41 +0200

    Add Frequency modal update

--- Js/Backend/Modules/Jobs/Views/AddFrequencyModal.jsx
+++ Js/Backend/Modules/Jobs/Views/AddFrequencyModal.jsx
@@ -16,8 +16,7 @@ class AddFrequencyModal extends Webiny.Ui.ModalComponent {
         const formProps = {
             ui: 'addFrequencyForm',
             api: '/entities/cron-manager/job-frequency',
-            fields: 'id, name, mask',
-            connectToRouter: true,
+            fields: 'id,name,mask',
             onSubmitSuccess: () => {
                 this.ui('frequencySelect').prepareOptions();
                 this.hide();
@@ -26,48 +25,47 @@ class AddFrequencyModal extends Webiny.Ui.ModalComponent {
 
         const listProps = {
             api: '/entities/cron-manager/job-frequency',
-            fields: '*',
-            connectToRouter: true
+            fields: '*'
         };
 
         return (
             <Ui.Modal.Dialog ref="dialog">
                 <Ui.Modal.Header title="Add Frequency"/>
                 <Ui.Modal.Body>
-
                     <Ui.Tabs.Tabs ui="myTabs" position="left">
                         <Ui.Tabs.Tab label="Create New">
 
                             <Ui.Form.Container {...formProps}>
-                        {() => (
-                            <Ui.Grid.Row>
-                                <Ui.Grid.Col all={12}>
-                                    <Ui.Input label="Name" name="name" validate="required"/>
-                                </Ui.Grid.Col>
-                                <Ui.Grid.Col all={12}>
-                                    <Ui.Input label="Mask" name="mask" validate="required,cronFrequency" description={this.getDescription}/>
-                                </Ui.Grid.Col>
-                            </Ui.Grid.Row>
-                        )}
+                                {() => (
+                                    <Ui.Grid.Row>
+                                        <Ui.Grid.Col all={12}>
+                                            <Ui.Input label="Name" name="name" validate="required"/>
+                                        </Ui.Grid.Col>
+                                        <Ui.Grid.Col all={12}>
+                                            <Ui.Input label="Mask" name="mask" validate="required,cronFrequency"
+                                                      description={this.getDescription}/>
+                                        </Ui.Grid.Col>
+                                    </Ui.Grid.Row>
+                                )}
                             </Ui.Form.Container>
                         </Ui.Tabs.Tab>
-
                         <Ui.Tabs.Tab label="Manage Existing">
                             <Ui.List.ApiContainer ui="maskList" {...listProps}>
-
                                 <Table.Table>
                                     <Table.Row>
-                                        <Table.Field name="Name" align="left" label="Name" sort="name" />
-                                        <Table.Field name="Mask" align="left" label="Mask" sort="mask" />
-
+                                        <Table.Field name="name" align="left" label="Name" sort="name"/>
+                                        <Table.Field name="mask" align="left" label="Mask" sort="mask"/>
                                         <Table.Actions>
-                                            <Table.DeleteAction/>
+                                            <Table.Action label="Delete" onClick={(data, $this) => {
+                                                // 1. $this je instanca akcije (Table.Action)
+                                                // 2. $this.props.actions - to su akcije koje mozes pozvati na listi, to se salje od liste kroz propse
+                                                // pogledaj kroz React plugin da vidis sta sve imas unutra. Delete automatski i refresha tablicu.
+                                                $this.props.actions.delete(data.id);
+                                            }}/>
                                         </Table.Actions>
                                     </Table.Row>
-
                                     <Table.Empty/>
                                 </Table.Table>
-
                                 <Ui.List.Pagination/>
                             </Ui.List.ApiContainer>
                         </Ui.Tabs.Tab>
